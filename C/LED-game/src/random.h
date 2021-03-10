13ddee030aeef263ab5b1e8a33ca1d9f8dcddd52
OlofPersson1994 <61694965+OlofPersson1994@users.noreply.github.com>
2021-03-10 20:21:26 +0100

    Delete C directory

--- C/LED-game/src/random.h
+++
@@ -1,6 +0,0 @@
-#ifndef RANDOM_H_
-#define RANDOM_H_
-
-int getRandommssecs(int min, int max);
-
-#endif
--- C/Step counter/src/pins.h
+++
@@ -1,7 +0,0 @@
-//Authors Olof Persson Karl Andersson
-#ifndef PINS_H_
-#define PINS_H_
-
-void initledButton();
-
-#endif
